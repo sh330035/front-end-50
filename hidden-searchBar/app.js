bddf3bd284bf614ca1132c7a028df2b9b77048bd
sh330035 <sh330035@gmail.com>
2022-12-27 16:05:41 +0800

    hidden search bar/ animation optimization

--- hidden-searchBar/app.js
+++ hidden-searchBar/app.js
@@ -1,6 +1,8 @@
+const container = document.querySelector(".container")
 const icon = document.querySelector(".icon")
 const search = document.querySelector(".search-bar")
 
 icon.addEventListener('click', ()=>{
     search.classList.toggle('active')
+    search.focus()
 })
